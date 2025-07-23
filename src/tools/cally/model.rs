429b6f06552055f458c7c9a4689c57a0413b70e1
Al Riddoch <alriddoch@googlemail.com>
2025-07-23 23:06:19 +0100

    Mark unimplememented

--- src/tools/cally/model.rs
+++ src/tools/cally/model.rs
@@ -223,6 +223,7 @@ impl Model {
     }
 
     pub fn setState(&mut self, state: usize, delay: f32) {
+        // unimplemented!();
         // check if this is really a new state
         // if state != self.state {
         //     if state == STATE_IDLE {
